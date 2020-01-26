ae7e0d78a6b9db10f0560695d5fbfaa976cc2d0e
worksofliam <mrliamallan@live.co.uk>
2020-01-26 18:20:15 -0500

    Change index file to do all work

--- index.js
+++ index.js
@@ -10,12 +10,42 @@ switch (undefined) {
     return;
 }
 
-start();
+work();
+async function work() {
+  var mode = '';
 
-async function start() {
+  console.log('');
+  switch (process.argv[2]) {
+    case 'models':
+    case 'procs':
+
+      mode = process.argv[2];
+      break;
+    default:
+      console.log('Please provide the correct mode. Either "models" or "procs".');
+      console.log('');
+      console.log('Models: Generates models for the specific table and any of it\'s foreign keys.');
+      console.log('\tindex.js models SAMPLE DEPARTMENT');
+      console.log('');
+      console.log('Procedures: Generates a class of methods for calling stored procedures in specified schema.');
+      console.log('\tindex.js procs QSYS');
+      console.log('');
+      return;
+  }
+
   console.log(`Connecting to ${process.env['ISYS']} with ${process.env['IUSER']}.`);
   console.log('')
   await db2.connect(`Driver=IBM i Access ODBC Driver;System=${process.env['ISYS']};UID=${process.env['IUSER']};Password=${process.env['IPASS']}`);
-  await ModelGenerator.getModel(process.argv[2], process.argv[3]);
-  await ModelGenerator.writeModels();
+
+  switch (mode) {
+    case 'models':
+      await ModelGenerator.getModel(process.argv[3], process.argv[4]);
+      await ModelGenerator.writeModels();
+      break;
+
+    case 'procs':
+      await ModelGenerator.getMethods(process.argv[3]);
+      await ModelGenerator.writeModels();
+      break;
+  }
 }
